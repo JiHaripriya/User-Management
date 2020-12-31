bca29eeb542c20daa40ba10eb53795c694ed4565
Sidharth S <sidharths@qburst.com>
2020-12-31 12:29:36 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PasswordComponent } from './authentication-layout/password/password.component';
 import { UsernameComponent } from './authentication-layout/username/username.component';
 
 const routes: Routes = [
-  {path: '', redirectTo:'/login', pathMatch:'full'},
-  {path: 'login', children: [
-    {path: '', component: UsernameComponent, pathMatch:'full'}
-  ]}
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'login', children: [
+      { path: '', component: UsernameComponent, pathMatch: 'full' },
+      { path: 'password', component: PasswordComponent }
+    ]
+  }
 ];
 
 @NgModule({
