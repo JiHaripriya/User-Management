a7c814e41fddd4d00438d1fc07c7c4fe763c2fa9
Sidharth S <sidharths@qburst.com>
2021-01-04 20:44:25 +0530

    change user details model

--- src/app/shared/models/user-details.model.ts
+++ src/app/shared/models/user-details.model.ts
@@ -1,11 +1,11 @@
 export class UserDetails {
     constructor(
-        private first_name: string,
-        private last_name: string,
-        private email: string,
+        public first_name: string,
+        public last_name: string,
+        public email: string,
         private role: string,
-        private status: string,
+        public status: string,
         private token?: string,
         private password?: string,
-    ) {}
+    ) { }
 }
