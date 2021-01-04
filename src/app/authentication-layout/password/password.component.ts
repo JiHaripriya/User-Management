092ecfd35c7c26dc05a99f3bc3e36c378973fec0
Haripriya <haripriya@qburst.com>
2021-01-04 17:36:10 +0530

    Role access fix

--- src/app/authentication-layout/password/password.component.ts
+++ src/app/authentication-layout/password/password.component.ts
@@ -7,6 +7,7 @@ import {
   AuthService,
 } from 'src/app/shared/services/api.service';
 import { ParticleService } from 'src/app/shared/services/particle.service';
+import { RoleGuardService } from 'src/app/shared/services/role-guard.service';
 import { UserDetailsService } from 'src/app/shared/services/user-details.service';
 
 @Component({
@@ -29,7 +30,8 @@ export class PasswordComponent implements OnInit, OnDestroy {
     private activatedRoute: ActivatedRoute,
     private authService: AuthService,
     public particleService: ParticleService,
-    private userDetailsApi: UserDetailsService
+    private userDetailsApi: UserDetailsService,
+    private roleService: RoleGuardService
   ) {}
 
   ngOnInit(): void {
--- src/app/shared/services/role-guard.service.ts
+++ src/app/shared/services/role-guard.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  Resolve,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { Subject } from 'rxjs';
+import { UserDetailsService } from './user-details.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RoleGuardService implements Resolve<string> {
+  userRole: string = '';
 
-  userRole: string = "";
-
-  constructor() {}
+  constructor(private userDetailsApi: UserDetailsService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    this.userRole = JSON.parse(localStorage.getItem('userDetails'))?.role;
+    this.userDetailsApi
+      .fetchUserDetails(JSON.parse(localStorage.getItem('userDetails'))?.email)
+      .subscribe(
+        (data) => (this.userRole = JSON.parse(JSON.stringify(data)).role)
+      );
     return this.userRole;
   }
 }
