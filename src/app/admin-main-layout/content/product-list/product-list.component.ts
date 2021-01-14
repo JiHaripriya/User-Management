6cccd306f29c733498835e7391fd78c833d5e499
Sidharth S <sidharths@qburst.com>
2021-01-14 18:15:15 +0530

    Product page

--- src/app/admin-main-layout/content/product-list/product-list.component.ts
+++ src/app/admin-main-layout/content/product-list/product-list.component.ts
@@ -7,6 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  searchItem;
+
+  products = [{
+    'name': 'Watch',
+    'price': '200',
+    'imgsrc': 'product-06.jpg'
+  },
+  {
+    'name': 'Shoe',
+    'price': '400',
+    'imgsrc': 'product-09.jpg'
+  },
+  {
+    'name': 'Dress',
+    'price': '300',
+    'imgsrc': 'product-12.jpg'
+  },
+  {
+    'name': 'Watch',
+    'price': '300',
+    'imgsrc': 'product-15.jpg'
+  },
+  {
+    'name': 'Watch',
+    'price': '300',
+    'imgsrc': 'product-15.jpg'
+  },
+  {
+    'name': 'Dress',
+    'price': '300',
+    'imgsrc': 'product-12.jpg'
+  },
+  {
+    'name': 'Watch',
+    'price': '200',
+    'imgsrc': 'product-06.jpg'
+  },
+  {
+    'name': 'Shoe',
+    'price': '400',
+    'imgsrc': 'product-09.jpg'
+  }
+  ];
+
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { CategoriesComponent } from './admin-main-layout/content/categories/cate
 import { SubCategoriesComponent } from './admin-main-layout/content/sub-categories/sub-categories.component';
 import { ProductFormsComponent } from './common/admin-forms/product-forms/product-forms.component';
 import { ProductListComponent } from './admin-main-layout/content/product-list/product-list.component';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
@@ -63,7 +64,8 @@ import { ProductListComponent } from './admin-main-layout/content/product-list/p
     ParticlesModule,
     NgbModule,
     CommonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    Ng2SearchPipeModule
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
   bootstrap: [AppComponent],
