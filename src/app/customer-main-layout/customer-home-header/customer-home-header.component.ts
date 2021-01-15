4708c97d4f3b308083c9817e0aab05c679cbb10f
Haripriya <haripriya@qburst.com>
2021-01-15 11:23:04 +0530

    Search box feature

--- src/app/customer-main-layout/customer-home-header/customer-home-header.component.ts
+++ src/app/customer-main-layout/customer-home-header/customer-home-header.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-customer-home-header',
   templateUrl: './customer-home-header.component.html',
-  styleUrls: ['./customer-home-header.component.css']
+  styleUrls: ['./customer-home-header.component.css'],
 })
 export class CustomerHeaderComponent implements OnInit {
-  
   fixHeader = false;
+  expandSearch = false;
+  @ViewChild('searchText') searchText: ElementRef;
 
   ngOnInit() {
     window.addEventListener('scroll', this.scrollEvent, true);
@@ -19,8 +20,16 @@ export class CustomerHeaderComponent implements OnInit {
 
   scrollEvent = (event: any): void => {
     let headerScroll = event.srcElement.scrollingElement.scrollTop;
-    if(headerScroll >= 30)  this.fixHeader = true;
+    if (headerScroll >= 30) this.fixHeader = true;
     else this.fixHeader = false;
+  };
+
+  onSearch() {
+    this.expandSearch = true;
   }
 
+  clearSearch() {
+    this.expandSearch = false;
+    this.searchText.nativeElement.value = '';
+  }
 }
